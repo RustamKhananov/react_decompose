50b7ca49ee5a9634311608b48f5708a23e4c76bb
Rustam Khananov <antmine2017@gmail.com>
2020-04-22 12:34:25 +0300

    Updates

---
+++ precache-manifest.f3af4a227886bd66abbdb386c41c60e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ef4fc179ef2d9e0f9ae72ea7e37525",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7c34ef830c03195a7f4",
+    "url": "./static/css/main.141cbc3c.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c34ef830c03195a7f4",
+    "url": "./static/js/main.71a804c4.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
